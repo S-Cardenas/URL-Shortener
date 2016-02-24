90bf5cae66dda8b1f85a3ae7cfb167c30d7d3d5a
edward&stefan@appacademy.io <appacademy@Apps-Mac-mini-129.local>
2016-02-24 15:38:15 -0500

    Add factory method for ShortenedUrl and associations

--- URLShortner/app/models/shortened_url.rb
+++ URLShortner/app/models/shortened_url.rb
@@ -1,6 +1,12 @@
-class ShortenedURL < ActiveRecord::Base
-  validates :long_url, presence: true
-  validates :short_url, :submitter_id, presence: true, uniqueness: true
+class ShortenedUrl < ActiveRecord::Base
+  validates :long_url, :submitter_id, presence: true
+  validates :short_url, presence: true, uniqueness: true
+  belongs_to(
+    :submitter,
+    class_name: 'User',
+    primary_key: :id,
+    foreign_key: :submitter_id
+  )
 
   def self.random_code
     code = SecureRandom.urlsafe_base64
@@ -14,7 +20,7 @@ class ShortenedURL < ActiveRecord::Base
   def self.create_for_user_and_long_url!(user, long_url)
     code = self.random_code
 
-    ShortenedURL.create!(submitter_id: user.id, long_url: long_url, short_url: code)
+    ShortenedUrl.create!(submitter_id: user.id, long_url: long_url, short_url: code)
   end
 
 end
--- URLShortner/app/models/user.rb
+++ URLShortner/app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
+  validates :email, :presence => true, :uniqueness => true
+
+  has_many(
+    :submitter_urls,
+    :class_name => 'ShortenedUrl',
+    primary_key: :id,
+    foreign_key: :submitter_id
+  )
 
 
 end
