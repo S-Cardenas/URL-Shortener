70a7175faa6ce3afe35d2e32899567283cfcd6b9
App Academy Student <appacademy@Apps-Mac-mini-129.local>
2016-02-24 14:16:39 -0500

    Finish associations exercise

---
+++ AssociationsExercise-master/skeleton/app/models/course.rb
@@ -0,0 +1,36 @@
+class Course < ActiveRecord::Base
+  has_many(
+    :enrollments,
+    class_name: 'Enrollment',
+    primary_key: :id,
+    foreign_key: :course_id
+  )
+
+  has_many(
+    :students,
+    through: :enrollments,
+    source: :user
+  )
+
+  has_many(
+    :child_courses,
+    class_name: "Course",
+    primary_key: :id,
+    foreign_key: :prereq_id
+  )
+
+  belongs_to(
+    :prerequisite,
+    class_name: 'Course',
+    primary_key: :id,
+    foreign_key: :prereq_id
+  )
+
+  belongs_to(
+    :instructor,
+    class_name: 'User',
+    primary_key: :id,
+    foreign_key: :instructor_id
+  )
+
+end
